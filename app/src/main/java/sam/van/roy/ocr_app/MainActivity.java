ff942f3a498da84af9b41a707ddcade0983b0427
SamVanRoy <saxo4sam@gmail.com>
2018-02-22 21:25:07 +0100

    basic functionality for taking a picture with the camera of your phone and run an ocr analysis from that picture

--- app/src/main/java/sam/van/roy/ocr_app/MainActivity.java
+++ app/src/main/java/sam/van/roy/ocr_app/MainActivity.java
@@ -1,14 +1,32 @@
 package sam.van.roy.ocr_app;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.GradientDrawable;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Looper;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
@@ -17,21 +35,53 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     Bitmap image; //our image
     private TessBaseAPI mTess; //Tess API reference
     String datapath = ""; //path to folder containing language data file
+    ImageView mImageView;
+    String mCurrentPhotoPath;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
 
     Button button;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init image
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image3);
+        mImageView = findViewById(R.id.imageView1);
+
+
+        Glide
+                .with(getApplicationContext())
+                .asBitmap()
+                .load(R.drawable.test_gsm_image)
+                .into(new SimpleTarget<Bitmap>(700,700) {
+
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        image = resource;
+                    }
+                });
+
+        Glide
+                .with(this)
+                .asBitmap()
+                .load(R.drawable.test_gsm_image)
+                .into(mImageView);
+
+
+
+
+//        image = BitmapFactory.decodeResource(getResources(), R.drawable.test_gsm_image);
 
         datapath = getFilesDir()+ "/tesseract/";
 
@@ -43,13 +93,46 @@ public class MainActivity extends AppCompatActivity {
         mTess = new TessBaseAPI();
         mTess.init(datapath, lang);
 
+
         button = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 processImage(view);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        setPic();
+    }
+
+    private void setPic() {
+        Glide
+                .with(getApplicationContext())
+                .asBitmap()
+                .load(mCurrentPhotoPath)
+                .into(new SimpleTarget<Bitmap>(700,700) {
+
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        image = resource;
+                    }
+                });
+        Glide.with(this).asBitmap().load(mCurrentPhotoPath).into(mImageView);
+        //image = BitmapFactory.decodeFile(mCurrentPhotoPath);
+//        Bitmap rotatedBitmap = ExifUtil.rotateBitmap(mCurrentPhotoPath, image);
+//        mImageView.setImageBitmap(rotatedBitmap);
     }
 
     private void copyFiles() {
@@ -103,4 +186,44 @@ public class MainActivity extends AppCompatActivity {
         TextView OCRTextView = (TextView) findViewById(R.id.textView);
         OCRTextView.setText(OCRresult);
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "sam.van.roy.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
 }
