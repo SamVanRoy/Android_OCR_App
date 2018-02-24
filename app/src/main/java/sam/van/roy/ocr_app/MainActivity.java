a5997768a2bb2768a831b9f48d78fa27a0933996
SamVanRoy <saxo4sam@gmail.com>
2018-02-24 14:40:01 +0100

    eerste stap richting croppen van image en ocr runnen van die image

--- app/src/main/java/sam/van/roy/ocr_app/MainActivity.java
+++ app/src/main/java/sam/van/roy/ocr_app/MainActivity.java
@@ -1,6 +1,12 @@
 package sam.van.roy.ocr_app;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,14 +16,17 @@ import android.graphics.drawable.GradientDrawable;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Looper;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.system.ErrnoException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +39,7 @@ import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -38,7 +48,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,36 +59,38 @@ public class MainActivity extends AppCompatActivity {
     String datapath = ""; //path to folder containing language data file
     ImageView mImageView;
     String mCurrentPhotoPath;
-
     static final int REQUEST_TAKE_PHOTO = 1;
-
 
     Button button;
     Button button2;
+
+    private CropImageView mCropImageView;
+
+    private Uri mCropImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImageView = findViewById(R.id.imageView1);
+        //mImageView = findViewById(R.id.imageView1);
 
-        Glide
-                .with(getApplicationContext())
-                .asBitmap()
-                .load(R.drawable.test_gsm_image)
-                .into(new SimpleTarget<Bitmap>(700,700) {
-
-                    @Override
-                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                        image = resource;
-                    }
-                });
-        Glide
-                .with(this)
-                .asBitmap()
-                .load(R.drawable.test_gsm_image)
-                .into(mImageView);
+//        Glide
+//                .with(getApplicationContext())
+//                .asBitmap()
+//                .load(R.drawable.test_gsm_image)
+//                .into(new SimpleTarget<Bitmap>(700,700) {
+//
+//                    @Override
+//                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+//                        image = resource;
+//                    }
+//                });
+//        Glide
+//                .with(this)
+//                .asBitmap()
+//                .load(R.drawable.test_gsm_image)
+//                .into(mImageView);
 
 
 
@@ -93,115 +107,194 @@ public class MainActivity extends AppCompatActivity {
         mTess = new TessBaseAPI();
         mTess.init(datapath, lang);
 
+        //Todo
+        mCropImageView = (CropImageView) findViewById(R.id.CropImageView);
 
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dispatchTakePictureIntent();
-            }
-        });
+
+//        button = findViewById(R.id.button1);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dispatchTakePictureIntent();
+//            }
+//        });
 
         button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 processImage(view);
+//                mCurrentPhotoUri = Uri.parse(mCurrentPhotoPath);
+//                CropImage.activity(mCurrentPhotoUri)
+//                        .start(MainActivity.this);
             }
         });
+    }
 
-//        final DragRectView view = (DragRectView) findViewById(R.id.dragRect);
-//
-//        if (null != view) {
-//            view.setOnUpCallback(new DragRectView.OnUpCallback() {
-//                @Override
-//                public void onRectFinished(final Rect rect) {
-//                    Toast.makeText(getApplicationContext(), "Rect is (" + rect.left + ", " + rect.top + ", " + rect.right + ", " + rect.bottom + ")",
-//                            Toast.LENGTH_LONG).show();
-//                }
-//            });
-//        }
+    /**
+     * On load image button click, start pick image chooser activity.
+     */
+    public void onLoadImageClick(View view) {
+        startActivityForResult(getPickImageChooserIntent(), 200);
+    }
+
+    /**
+     * Crop the image and set it back to the cropping view.
+     */
+    public void onCropImageClick(View view) {
+        Bitmap cropped = mCropImageView.getCroppedImage(500, 500);
+        if (cropped != null)
+            mCropImageView.setImageBitmap(cropped);
+        image = cropped;
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        setPic();
+        if (resultCode == Activity.RESULT_OK) {
+            Uri imageUri = getPickImageResultUri(data);
+
+            // For API >= 23 we need to check specifically that we have permissions to read external storage,
+            // but we don't know if we need to for the URI so the simplest is to try open the stream and see if we get error.
+            boolean requirePermissions = false;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                    isUriRequiresPermissions(imageUri)) {
+
+                // request permissions and handle the result in onRequestPermissionsResult()
+                requirePermissions = true;
+                mCropImageUri = imageUri;
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            }
+
+            if (!requirePermissions) {
+                mCropImageView.setImageUriAsync(imageUri);
+            }
+        }
     }
 
-    private void setPic() {
-        assignBitmapWithGlide(mCurrentPhotoPath);
-        Glide.with(this).asBitmap().load(mCurrentPhotoPath).into(mImageView);
-        //image = BitmapFactory.decodeFile(mCurrentPhotoPath);
-//        Bitmap rotatedBitmap = ExifUtil.rotateBitmap(mCurrentPhotoPath, image);
-//        mImageView.setImageBitmap(rotatedBitmap);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (mCropImageUri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mCropImageView.setImageUriAsync(mCropImageUri);
+        } else {
+            Toast.makeText(this, "Required permissions are not granted", Toast.LENGTH_LONG).show();
+        }
     }
 
-    private void assignBitmapWithGlide(String location) {
-        Glide
-                .with(getApplicationContext())
-                .asBitmap()
-                .load(location)
-                .into(new SimpleTarget<Bitmap>(700,700) {
+    /**
+     * Create a chooser intent to select the source to get image from.<br/>
+     * The source can be camera's (ACTION_IMAGE_CAPTURE) or gallery's (ACTION_GET_CONTENT).<br/>
+     * All possible sources are added to the intent chooser.
+     */
+    public Intent getPickImageChooserIntent() {
 
-                    @Override
-                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                        image = resource;
-                    }
-                });
+        // Determine Uri of camera image to save.
+        Uri outputFileUri = getCaptureImageOutputUri();
+
+        List<Intent> allIntents = new ArrayList<>();
+        PackageManager packageManager = getPackageManager();
+
+        // collect all camera intents
+        Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+        for (ResolveInfo res : listCam) {
+            Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(res.activityInfo.packageName);
+            if (outputFileUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            }
+            allIntents.add(intent);
+        }
+
+        // collect all gallery intents
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        List<ResolveInfo> listGallery = packageManager.queryIntentActivities(galleryIntent, 0);
+        for (ResolveInfo res : listGallery) {
+            Intent intent = new Intent(galleryIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(res.activityInfo.packageName);
+            allIntents.add(intent);
+        }
+
+        // the main intent is the last in the list (fucking android) so pickup the useless one
+        Intent mainIntent = allIntents.get(allIntents.size() - 1);
+        for (Intent intent : allIntents) {
+            if (intent.getComponent().getClassName().equals("com.android.documentsui.DocumentsActivity")) {
+                mainIntent = intent;
+                break;
+            }
+        }
+        allIntents.remove(mainIntent);
+
+        // Create a chooser from the main intent
+        Intent chooserIntent = Intent.createChooser(mainIntent, "Select source");
+
+        // Add all other intents
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, allIntents.toArray(new Parcelable[allIntents.size()]));
+
+        return chooserIntent;
     }
 
-    private void copyFiles() {
+    /**
+     * Get URI to image received from capture by camera.
+     */
+    private Uri getCaptureImageOutputUri() {
+        Uri outputFileUri = null;
+        File getImage = getExternalCacheDir();
+        if (getImage != null) {
+            outputFileUri = Uri.fromFile(new File(getImage.getPath(), "pickImageResult.jpeg"));
+        }
+        return outputFileUri;
+    }
+
+    /**
+     * Get the URI of the selected image from {@link #getPickImageChooserIntent()}.<br/>
+     * Will return the correct URI for camera and gallery image.
+     *
+     * @param data the returned data of the activity result
+     */
+    public Uri getPickImageResultUri(Intent data) {
+        boolean isCamera = true;
+        if (data != null && data.getData() != null) {
+            String action = data.getAction();
+            isCamera = action != null && action.equals(MediaStore.ACTION_IMAGE_CAPTURE);
+        }
+        return isCamera ? getCaptureImageOutputUri() : data.getData();
+    }
+
+    /**
+     * Test if we can open the given Android URI to test if permission required error is thrown.<br>
+     */
+    public boolean isUriRequiresPermissions(Uri uri) {
         try {
-            //location we want the file to be at
-            String filepath = datapath + "/tessdata/eng.traineddata";
-
-            //get access to AssetManager
-            AssetManager assetManager = getAssets();
-
-            //open byte streams for reading/writing
-            InputStream instream = assetManager.open("tessdata/eng.traineddata");
-            OutputStream outstream = new FileOutputStream(filepath);
-
-            //copy the file to the location specified by filepath
-            byte[] buffer = new byte[1024];
-            int read;
-            while ((read = instream.read(buffer)) != -1) {
-                outstream.write(buffer, 0, read);
-            }
-            outstream.flush();
-            outstream.close();
-            instream.close();
-
+            ContentResolver resolver = getContentResolver();
+            InputStream stream = resolver.openInputStream(uri);
+            stream.close();
+            return false;
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void checkFile(File dir) {
-        //directory does not exist, but we can successfully create it
-        if (!dir.exists()&& dir.mkdirs()){
-            copyFiles();
-        }
-        //The directory exists, but there is no data file in it
-        if(dir.exists()) {
-            String datafilepath = datapath+ "/tessdata/eng.traineddata";
-            File datafile = new File(datafilepath);
-            if (!datafile.exists()) {
-                copyFiles();
+            if (e.getCause() instanceof ErrnoException) {
+                return true;
             }
+        } catch (Exception e) {
         }
+        return false;
     }
 
-    public void processImage(View view){
-        String OCRresult = null;
-        mTess.setImage(image);
-        OCRresult = mTess.getUTF8Text();
-        TextView OCRTextView = (TextView) findViewById(R.id.textView);
-        OCRTextView.setText(OCRresult);
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        setPictureInPlace();
+//    }
+
+    private void setPictureInPlace() {
+        //is dit nodig?
+        assignBitmapWithGlide(mCurrentPhotoPath);
+
+        Glide.with(this).asBitmap().load(mCurrentPhotoPath).into(mImageView);
     }
+
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -239,6 +332,76 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
+    }
+
+    //Tesseract
+    private void copyFilesTrainedData() {
+        try {
+            //location we want the file to be at
+            String filepath = datapath + "/tessdata/eng.traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading/writing
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Tesseract
+    private void checkFile(File dir) {
+        //directory does not exist, but we can successfully create it
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFilesTrainedData();
+        }
+        //The directory exists, but there is no data file in it
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFilesTrainedData();
+            }
+        }
+    }
+
+    //Tesseract
+    public void processImage(View view){
+        String OCRresult = null;
+        mTess.setImage(image);
+        OCRresult = mTess.getUTF8Text();
+        TextView OCRTextView = (TextView) findViewById(R.id.textView);
+        OCRTextView.setText(OCRresult);
+    }
+
+    //utilityMethod with Glide
+    private void assignBitmapWithGlide(String location) {
+        Glide
+                .with(getApplicationContext())
+                .asBitmap()
+                .load(location)
+                .into(new SimpleTarget<Bitmap>(700,700) {
+
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        image = resource;
+                    }
+                });
     }
 
 
